2efe3c1f5e865747fcf9fc1d97b226d9a3262697
pedrorc12 <pedro.rc12@hotmail.com>
2020-12-30 19:00:13 +0000

    Iterador em Orderm implementado e documentação

--- Code/BST.cpp
+++ Code/BST.cpp
@@ -5,6 +5,9 @@
 #include <cstddef>
 #include "BST.h"
 
+///////Nodes methods
+
+///Node empty constructor
 template<class T>
 Node<T>::Node() {
     T empty;
@@ -14,6 +17,7 @@ Node<T>::Node() {
 
 }
 
+///Create a Node with the element
 template<class T>
 Node<T>::Node(T element) {
     node = element;
@@ -22,6 +26,7 @@ Node<T>::Node(T element) {
 
 }
 
+///Recursivly insert a new element in a node
 template<class T>
 void Node<T>::insert(Node *element) {
     if(left == nullptr && element->getElement() < node) this->left = element;
@@ -30,18 +35,23 @@ void Node<T>::insert(Node *element) {
     else right->insert(element);
 }
 
+///////Binary search tree methods
+
+///Binary tree empty constructor
 template<class T>
 BST<T>::BST() {
     Node<T>* empty = Node<T>();
     root = empty;
 }
 
+///BST constructor with a root
 template<class T>
 BST<T>::BST(T root) {
     Node<T> *newNode = new Node<T>(root);
     this->root = newNode;
 }
 
+///Insert a new element to the BST and allocate a new node
 template<class T>
 void BST<T>::insert(T &newElement) {
     Node<T>* newNode = new Node<T>(newElement);
@@ -50,6 +60,7 @@ void BST<T>::insert(T &newElement) {
     root->insert(newNode);
 }
 
+///Recursive function that return the element based on the comparable or the empty Node
 template<class T>
 T BST<T>::find(Node<T> *node, T comparable) {
     T empty;
@@ -59,11 +70,13 @@ T BST<T>::find(Node<T> *node, T comparable) {
     else return node->getElement();
 }
 
+///BST find function with a comparable that returns the element or the empty Node
 template<class T>
 T BST<T>::find(T comparable) {
     return find(root, comparable);
 }
 
+///Recursively deallocate the nodes
 template<class T>
 void BST<T>::clearTree(Node<T> *nodeToClear) {
     if(nodeToClear->left != nullptr) clearTree(nodeToClear->left);
@@ -71,7 +84,52 @@ void BST<T>::clearTree(Node<T> *nodeToClear) {
     delete nodeToClear;
 }
 
+///Deconstructor clears the tree deallocating the nodes
 template<class T>
 BST<T>::~BST<T>() {
     clearTree(root);
+}
+
+///////Iterator methods
+
+///Iterator constructor using a BST
+template<class T>
+inOrderIterator<T>::inOrderIterator(BST<T> &bst) {
+    if (bst.root != nullptr) copyBST(bst.root);
+}
+
+///Recursively travel through the BST in reverse so the stack stays in order
+template<class T>
+void inOrderIterator<T>::copyBST(Node<T> *node) {
+    if(node->right != nullptr) {
+        copyBST(node->right);
+    }
+    binaryTree.push(node->getElement());
+    if(node->left != nullptr){
+        copyBST(node->left);
+    }
+}
+
+///Returns the element in the Nodes
+template<class T>
+T inOrderIterator<T>::operator*() {
+    return binaryTree.top();
+}
+
+///Returns the element in the Nodes
+template<class T>
+T inOrderIterator<T>::retrieve() {
+    return binaryTree.top();
+}
+
+///Goes to the next element by poping the stack
+template<class T>
+void inOrderIterator<T>::advance() {
+    if(!binaryTree.empty()) binaryTree.pop();
+}
+
+///Return true if the stack is empty
+template<class T>
+bool inOrderIterator<T>::isAtEnd() {
+    return binaryTree.empty();
 }
--- Code/BST.h
+++ Code/BST.h
@@ -1,9 +1,18 @@
 #pragma once
+#include <stack>
 
+template<class T> class inOrderIterator;
+
+/**
+ * class Node
+ * @param node - the element to be include inside de node
+ * @param left - pointer to the left node
+ * @param right - pointer to the right node
+ */
 template <class T>
 class Node{
-public:
     T node;
+public:
     Node* left;
     Node* right;
     Node();
@@ -11,13 +20,19 @@ public:
     T getElement() {return node;}
     void insert(Node* element);
     bool operator < (Node<T> &rhs) { return node < rhs.node;}
-    bool operator == (Node<T> rhs) {return node == rhs.node;}
+    bool operator == (Node<T> rhs) {return !(node < rhs.node) && !(rhs.node < node);}
 };
 
+/**
+ * class BST - binary search tree
+ * @param root - the pointer to de first root of the tree
+ */
 template <class T>
 class BST {
+private:
     Node<T> *root;
     T find(Node<T> *node, T comparable);
+    friend class inOrderIterator<T>;
 public:
     BST();
     BST(T root);
@@ -27,4 +42,18 @@ public:
     T find(T comparable);
 };
 
-
+/**
+ * class inOrderIterator - iterator to a binary tree in order
+ * @param binaryTree - stack on witch the tree is stored
+ */
+template<class T>
+class inOrderIterator{
+    std::stack<T> binaryTree;
+    void copyBST(Node<T> *node);
+public:
+    inOrderIterator(BST<T> &bst);
+    void advance();
+    T retrieve();
+    T operator *();
+    bool isAtEnd();
+};
